acee922bd9be1fda50dcddb668a24ac2ea811511
AunchisaS <creamaunchisa09@gmail.com>
2023-02-22 15:23:34 +0700

    add authentication

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 //Rout file
 const hospitals =require('./routes/hospitals');
+const auth = require('./routes/auth');
 
 //Load env vars
 dotenv.config({path:"./config/config.env"});
@@ -15,7 +17,10 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
 
+//CoolkieParser
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log("Server running in ",process.env.NODE_ENV," mode on port ",PORT));
